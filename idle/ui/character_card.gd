42a44dbcbcfcd260f6788fe2a2f4ba84fc9e70d9
Nicholas Sacayan <nksacayan@gmail.com>
2025-05-05 00:55:37 -0700

    Character card works

--- idle/ui/character_card.gd
+++ idle/ui/character_card.gd
@@ -3,14 +3,18 @@ extends PanelContainer
 @export var character_name_label: Label
 @export var character_stats_label: Label
 
-const CHARACTER_STAT_TEMPLATE = "%s: %d"
+const CHARACTER_STAT_TEMPLATE = "%s: %d\n"
 
-var character: Character
+@export var character: Character
+
+func _ready():
+	update_ui()
 
 func update_ui():
 	character_name_label.text = character.character_name
+	update_character_stats_label()
 
 func update_character_stats_label():
 	character_stats_label.text = ""
-	for stat: BaseStat.StatNames in BaseStat.StatNames.values():
-		character_stats_label.text += CHARACTER_STAT_TEMPLATE % [BaseStat.StatNames.keys()[stat], character.character_stats.base_stats[stat]]
+	for stat: BaseStat in character.base_stats:
+		character_stats_label.text += CHARACTER_STAT_TEMPLATE % [BaseStat.StatNames.keys()[stat.stat_name], stat.stat_value]
