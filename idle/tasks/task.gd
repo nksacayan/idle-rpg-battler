8dfa1c0017c44085722d44e1f8ab2fd57ecb93e0
Nicholas Sacayan <nicholas.sacayan@vetsez.com>
2025-12-13 14:25:28 -0800

    going nuclear i dont like it

--- idle/tasks/task.gd
+++
@@ -1,5 +0,0 @@
-extends Resource
-class_name Task
-
-@export var task_name: String
-@export var stats_and_exp: Dictionary[BaseStat.StatNames, int]
--- idle/tasks/task_library/task_library.gd
+++
@@ -1,3 +0,0 @@
-extends Node
-
-@export var task_library: Array[Task]
