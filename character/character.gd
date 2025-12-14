27b1d7d2a4a460c5e7b73268ce2ed45ee481bb67
Nicholas Sacayan <nicholas.sacayan@vetsez.com>
2025-12-13 22:10:46 -0800

    Starts making character implementation with stats

--- character/character.gd
+++ character/character.gd
@@ -1,11 +1,30 @@
-extends Node
+extends RefCounted
+class_name Character
 
+static var character_stat_definitions: Array[CharacterStatDefinition]
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var character_name: String
+var character_stats: Array[CharacterStat]
 
+var health := Health.new()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+# Note: Not a fan of this way of static initializing, but it's the simplest
+# Wanted to have something more configurable in the editor with exported definitions
+# but doing that complicated the architecture
+static func _static_init() -> void:
+	character_stat_definitions = [
+		preload("res://character/stat_definitions/strength.tres"),
+		preload("res://character/stat_definitions/dexterity.tres"),
+		preload("res://character/stat_definitions/constitution.tres"),
+		preload("res://character/stat_definitions/wisdom.tres"),
+		preload("res://character/stat_definitions/intelligence.tres"),
+		preload("res://character/stat_definitions/charisma.tres"),
+	]
+
+func _init(p_character_name := "default_character_name") -> void:
+	character_name = p_character_name
+	_init_character_stats()
+
+func _init_character_stats() -> void:
+	for definition in character_stat_definitions:
+		character_stats.append(CharacterStat.new(definition))
