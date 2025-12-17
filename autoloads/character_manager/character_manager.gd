dfda12c32e420272f3ef980ab35aab1c4710ac5a
Nicholas Sacayan <nksacayan@gmail.com>
2025-12-16 16:09:13 -0800

    character manager signals

--- autoloads/character_manager/character_manager.gd
+++ autoloads/character_manager/character_manager.gd
@@ -1,10 +1,16 @@
 extends Node
 class_name CharacterManager
 
+signal character_created(character: Character)
+
 @export var characters: Array[Character]
 
 func create_character(p_character_name: String = "") -> void:
+    var new_character: Character
     if p_character_name.is_empty():
-        characters.append(Character.new())
+        new_character = Character.new()
+        characters.append(new_character)
     else:
-        characters.append(Character.new(p_character_name))
+        new_character = Character.new(p_character_name)
+        characters.append(new_character)
+    character_created.emit(new_character)
