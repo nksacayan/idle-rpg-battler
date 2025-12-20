f8ab45563136cb35645dccedf04e3ef0491eda9a
Nicholas Sacayan <nicholas.sacayan@vetsez.com>
2025-12-20 14:49:23 -0800

    Draggable tasks

--- autoloads/character_task_manager/character_task_manager.gd
+++ autoloads/character_task_manager/character_task_manager.gd
@@ -1,6 +1,8 @@
 extends Node
 class_name CharacterTaskManager
 
+signal character_task_added(p_character_task: CharacterTask)
+
 @export var character_tasks: Array[CharacterTask]
 # TODO: Change this and make it rely on character or task or something
 const VALUE_PER_TICK: int = 1
@@ -9,3 +11,8 @@ const VALUE_PER_TICK: int = 1
 func _physics_process(delta: float) -> void:
 	for character_task in character_tasks:
 		character_task.progress += VALUE_PER_TICK * delta
+
+func create_character_task(p_character: Character, p_task: TaskDefinition) -> void:
+	var character_task := CharacterTask.new(p_character, p_task)
+	character_tasks.append(character_task)
+	character_task_added.emit(character_task)
--- tasks/UI/task_card.gd
+++ tasks/UI/task_card.gd
@@ -15,3 +15,14 @@ func _update_labels() -> void:
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	_update_labels()
+
+func _get_drag_data(_at_position: Vector2) -> Variant:
+	var preview_card: TaskCard = duplicate()
+	var preview_wrapper = CenterContainer.new()
+
+	preview_card.custom_minimum_size = self.size
+	preview_wrapper.use_top_left = true
+	preview_wrapper.add_child(preview_card)
+
+	set_drag_preview(preview_wrapper)
+	return task
