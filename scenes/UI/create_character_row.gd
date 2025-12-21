57315b4502fa73180c3363f4d9220c70c83e9265
Nicholas Sacayan <nicholas.sacayan@vetsez.com>
2025-12-20 19:11:00 -0800

    minor update to character creation

--- scenes/UI/create_character_row.gd
+++ scenes/UI/create_character_row.gd
@@ -4,7 +4,10 @@ extends HBoxContainer
 
 func call_create_character() -> void:
 	CharacterManagerAutoload.create_character(character_name_line.text)
+	character_name_line.clear()
 
 func _on_button_pressed() -> void:
 	call_create_character()
-	character_name_line.clear()
+
+func _on_line_edit_text_submitted(_new_text: String) -> void:
+	call_create_character()
