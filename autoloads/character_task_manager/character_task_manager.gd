4f65b0ab2598be7f421059b3fad37d5d5a2b8e05
Nicholas Sacayan <nicholas.sacayan@vetsez.com>
2025-12-20 19:18:47 -0800

    training works, adjusted values for faster testing

--- autoloads/character_task_manager/character_task_manager.gd
+++ autoloads/character_task_manager/character_task_manager.gd
@@ -5,7 +5,7 @@ signal character_task_added(p_character_task: CharacterTask)
 
 @export var character_tasks: Array[CharacterTask]
 # TODO: Change this and make it rely on character or task or something
-const VALUE_PER_TICK: int = 1
+const VALUE_PER_TICK: int = 100
 
 # Physics accuracy not necessary, but putting it here to reduce number of ticks for performance
 func _physics_process(delta: float) -> void:
