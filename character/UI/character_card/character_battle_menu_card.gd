3e74ace7c56f53363a56c7aa9a45a6397ea34abe
Nicholas Sacayan <nksacayan@gmail.com>
2025-12-26 17:31:01 -0800

    character battle cards work

--- character/UI/character_card/character_battle_menu_card.gd
+++ character/UI/character_card/character_battle_menu_card.gd
@@ -1,16 +1,11 @@
 extends Control
 
-# Connect this signal in editor
-signal card_delete_requested
-
 @export var character_card_root: CharacterCard
 @onready var character: Character = character_card_root.character
 @onready var name_label: Label = %NameLabel
 
 func _ready() -> void:
 	name_label.text = character.character_name
-	CharacterManagerAutoload.removed_from_battle_team.connect(_handle_battle_team_character_removed)
 
-func _handle_battle_team_character_removed(p_character: Character) -> void:
-	if p_character == character:
-		card_delete_requested.emit()
+func remove_from_battle_team() -> void:
+	CharacterManagerAutoload.remove_from_battle_team(character)
