ee0df0c46b547f2dea11a2b5ef40f96547215b07
Nicholas Sacayan <nksacayan@gmail.com>
2025-12-26 16:45:45 -0800

    okay preview card works now

--- character/UI/character_card/character_battle_menu_card.gd
+++ character/UI/character_card/character_battle_menu_card.gd
@@ -4,6 +4,5 @@ extends Control
 @onready var character: Character = character_card_root.character
 @onready var name_label: Label = %NameLabel
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	name_label.text = character.character_name
