f2e2394a8a7efad7219f4db7558856aa41e2d086
Nicholas Sacayan <nicholas.sacayan@vetsez.com>
2026-01-01 17:13:03 -0800

    refactor to use @tool

--- character/character_data.gd
+++ character/character_data.gd
@@ -1,3 +1,5 @@
+@tool
+# Testing using tool to help resource init in editor
 extends Resource
 class_name CharacterData
 
@@ -10,17 +12,19 @@ enum STAT_NAMES {
 	CHARISMA
 }
 
-const DEFAULT_CHARACTER_NAME := "DEFAULT NAME"
-@export var character_name: String = DEFAULT_CHARACTER_NAME
-@export var stats: Dictionary[STAT_NAMES, LeveledStat] = {
-	STAT_NAMES.STRENGTH: LeveledStat.new(),
-	STAT_NAMES.DEXTERITY: LeveledStat.new(),
-	STAT_NAMES.CONSTITUTION: LeveledStat.new(),
-	STAT_NAMES.WISDOM: LeveledStat.new(),
-	STAT_NAMES.INTELLIGENCE: LeveledStat.new(),
-	STAT_NAMES.CHARISMA: LeveledStat.new()
-}
+const DEFAULT_NAME := "DEFAULT NAME"
+@export var character_name: String = DEFAULT_NAME
+@export var stats: Dictionary[STAT_NAMES, LeveledStat]
 
 # Will probably have to change init or some setup later to get stats to serialize?
-func _init(p_character_name: String = DEFAULT_CHARACTER_NAME) -> void:
+func _init(p_character_name: String = DEFAULT_NAME) -> void:
 	character_name = p_character_name
+	if stats.is_empty():
+		stats = {
+			STAT_NAMES.STRENGTH: LeveledStat.new(),
+			STAT_NAMES.DEXTERITY: LeveledStat.new(),
+			STAT_NAMES.CONSTITUTION: LeveledStat.new(),
+			STAT_NAMES.WISDOM: LeveledStat.new(),
+			STAT_NAMES.INTELLIGENCE: LeveledStat.new(),
+			STAT_NAMES.CHARISMA: LeveledStat.new()
+		}
