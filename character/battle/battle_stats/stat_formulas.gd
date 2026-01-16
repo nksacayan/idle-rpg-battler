f3dde5bd3565f0d9bb3ad3b1193e3c4a6ddedccb
Nicholas Sacayan <nksacayan@gmail.com>
2026-01-15 18:04:57 -0800

    Updates stat formulas for resources

--- character/battle/battle_stats/stat_formulas.gd
+++ character/battle/battle_stats/stat_formulas.gd
@@ -14,14 +14,17 @@ static var resource_formula_helpers: Dictionary[BattleCharacter.RESOURCE_NAMES, 
 	BattleCharacter.RESOURCE_NAMES.STAMINA: StatFormulaHelper.new(calc_per_constitution, [CharacterData.STAT_NAMES.CONSTITUTION]),
 	BattleCharacter.RESOURCE_NAMES.MAGIC: StatFormulaHelper.new(calc_per_intelligence, [CharacterData.STAT_NAMES.INTELLIGENCE]),
 }
-static func calc_per_constitution(character_stats: Dictionary[CharacterData.STAT_NAMES, LeveledStat]) -> int:
+static func calc_per_constitution(p_base_stats: Array[BaseStat]) -> int:
 	var value_per_constitution := 10
-	var value := _calculate_value_per_character_stat(character_stats[CharacterData.STAT_NAMES.CONSTITUTION], value_per_constitution)
+	var con_stat: BaseStat = _find_stat_by_name(p_base_stats, _get_base_stat_name(CharacterData.STAT_NAMES.CONSTITUTION))
+	var value := con_stat.value * value_per_constitution
 	return value
 
-static func calc_per_intelligence(character_stats: Dictionary[CharacterData.STAT_NAMES, LeveledStat]) -> int:
+static func calc_per_intelligence(p_base_stats: Array[BaseStat]) -> int:
 	var value_per_intelligence := 10
-	return _calculate_value_per_character_stat(character_stats[CharacterData.STAT_NAMES.INTELLIGENCE], value_per_intelligence)
+	var int_stat: BaseStat = _find_stat_by_name(p_base_stats, _get_base_stat_name(CharacterData.STAT_NAMES.INTELLIGENCE))
+	var value := int_stat.value * value_per_intelligence
+	return value
 
 # battle stat formulas
 # these have to be updated/upkept manually
@@ -91,9 +94,6 @@ static func calc_speed(p_base_stats: Array[BaseStat]) -> int:
 	return speed_value
 
 # helpers can deviate as only the above will be callables
-static func _calculate_value_per_character_stat(p_base_stat: LeveledStat, value_per_stat: int) -> int:
-	return p_base_stat.stat.value * value_per_stat
-
 static func _find_stat_by_name(p_base_stats: Array[BaseStat], p_stat_name: String) -> BaseStat:
 	var filtered := p_base_stats.filter(func(p_base_stat) -> bool: return p_base_stat.stat_name == p_stat_name)
 	if filtered.size() > 0:
