5b166e7814b6cdcc03a125565dd1d33cf9812493
Nicholas Sacayan <nksacayan@gmail.com>
2026-01-16 00:11:08 -0800

    was working on event bus for modal layer

---
+++ autoloads/event_bus/event_bus.gd
@@ -0,0 +1,6 @@
+extends Node
+class_name EventBus
+
+# do this until i have a gazillion
+@warning_ignore("UNUSED_SIGNAL")
+signal request_show_character_details(p_character_data: CharacterData)
---
+++ character/user_interface/idle/character_card/implementations/character_detail_button.gd
@@ -0,0 +1,5 @@
+extends CharacterCardComponent
+
+func _enter_tree() -> void:
+	provide_drag_impl = false
+	provide_drop_impl = false
