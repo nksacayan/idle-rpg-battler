2def3ea8e10adddab7bf312a4e1049a7a4cb83c1
Nicholas Sacayan <nicholas.sacayan@vetsez.com>
2026-01-19 10:29:00 -0800

    deletes old button

--- character/user_interface/idle/character_card/implementations/character_detail_button.gd
+++
@@ -1,8 +0,0 @@
-extends CharacterCardComponent
-
-func _enter_tree() -> void:
-	provide_drag_impl = false
-	provide_drop_impl = false
-
-func _request_display_character_detail() -> void:
-	EventBusAutoload.request_show_character_details.emit(character)
