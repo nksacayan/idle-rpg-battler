1903761d1d6ffca3357b65981ab073fa3968d7a3
Nicholas Sacayan <nksacayan@gmail.com>
2026-01-24 12:30:06 -0800

    working on unlock details

--- character/user_interface/idle/character_card/components/battle_character_components/command_unlocks_display_component.gd
+++ character/user_interface/idle/character_card/components/battle_character_components/command_unlocks_display_component.gd
@@ -12,13 +12,12 @@ var available_unlocks: Array[CommandUnlock]:
 
 var selected_unlock: CommandUnlock:
 	set(p_command_unlock):
-		pass
+		_provide_selected_unlock()
 
 func _set_character(p_character: BattleCharacter) -> void:
 	super (p_character)
 	_provide_available_unlocks()
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	_provide_available_unlocks()
 
@@ -39,3 +38,19 @@ func _provide_available_unlocks() -> void:
 	
 	battle_command_buttons.battle_commands = filtered_commands
 
+func _provide_selected_unlock() -> void:
+	if selected_unlock:
+		battle_command_detail.battle_command = selected_unlock.command
+	else:
+		battle_command_detail.battle_command = null
+
+func _on_unlockable_command_selected(p_command: BattleCommand) -> void:
+	var unlockables_of_command: Array[CommandUnlock]
+	unlockables_of_command.assign(available_unlocks.filter(
+		func(unlock): return unlock.command == p_command
+	))
+	if unlockables_of_command.size() <= 0:
+		push_error("Somehow selected a command without a matching unlock", p_command)
+		selected_unlock = null
+		return
+	selected_unlock = unlockables_of_command[0]
