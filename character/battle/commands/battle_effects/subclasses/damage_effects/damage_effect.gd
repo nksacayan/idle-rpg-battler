a4dc3dec2efd940fb22c5772e7e9b241f1ca6bfa
Nicholas Sacayan <nksacayan@gmail.com>
2026-01-27 18:14:32 -0800

    redoes test enemies and adds logs to damage plus fixes

--- character/battle/commands/battle_effects/subclasses/damage_effects/damage_effect.gd
+++ character/battle/commands/battle_effects/subclasses/damage_effects/damage_effect.gd
@@ -17,20 +17,20 @@ func apply_effect(_p_source_character: BattleCharacter, _p_targets: Array[Battle
 	var total_power: int = (
 		base_power + 
 		int(
-			_p_source_character.battle_stats[additive_stat].stat_value.value *
+			_p_source_character.battle_stats[additive_stat].value *
 			additive_stat_factor
 		)
 	)
-	print("total power from ", _p_source_character, " is ", total_power)
 	var defending_stat: BattleCharacter.BATTLE_STAT_NAMES = (
 		BattleCharacter.BATTLE_STAT_NAMES.PHYSICAL_DEFENSE if
 		damage_type == DAMAGE_TYPES.PHYSICAL else
 		BattleCharacter.BATTLE_STAT_NAMES.MAGICAL_DEFENSE
 	)
 	for target: BattleCharacter in _p_targets:
-		var defending_stat_value: int = target.battle_stats[defending_stat].stat_value.value
+		var defending_stat_value: int = target.battle_stats[defending_stat].value
 		print("defending value for ", target, " is ", defending_stat_value)
 		var damage = max(total_power - defending_stat_value, 0)
-		target.character_resources[BattleCharacter.RESOURCE_NAMES.HEALTH].current -= damage
+		target.character_resources[BattleCharacter.RESOURCE_NAMES.HEALTH].current_value -= damage
 		print("dealt ", damage, " to ", target);
 	print_debug("applying effect ", effect_name, "from source ", _p_source_character, "to target ", _p_targets)
+
