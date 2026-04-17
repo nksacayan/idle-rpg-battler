2dfe6c3862e72dceadb26d1a3dbf639705fc6898
Nicholas Sacayan <nksacayan@gmail.com>
2026-04-16 18:35:06 -0700

    make to_dict from_dict methods in character data

--- scenes/battle/multiplayer/multiplayer_battle.gd
+++ scenes/battle/multiplayer/multiplayer_battle.gd
@@ -4,8 +4,9 @@ class_name MultiplayerBattle
 const PORT = 4433
 const ADDRESS = "127.0.0.1"
 
-var _team_1: Array[BattleCharacter]
-var _team_2: Array[BattleCharacter]
+# Testing, will be fed in before entering multiplayer
+@export var _team_1: Array[CharacterData]
+@export var _team_2: Array[CharacterData]
 
 func _ready():
 	# These signals are emitted by the global MultiplayerAPI
